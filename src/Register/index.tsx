98728d581633d5ae00fe2c81eb5076de4ac2462f
Renato Ruiz <renatoruiz2607@gmail.com>
2021-06-14 14:35:43 -0300

    Configurados requires e erros nos formulários de Registro utilizando yup e react-hook-form

--- src/Register/index.tsx
+++ src/Register/index.tsx
@@ -1,26 +1,24 @@
 import React, { useState } from 'react'
-import { Container, Body, TransparentLayer, GlobalStyle } from '../Login/styles'
+import { Container, Body, TransparentLayer, GlobalStyle, RequiredField } from '../Login/styles'
 import LoadRandomPicture from '../Login/backgroundImage';
 import { RegisterStyle } from './styles'
 import { useHistory } from 'react-router'
 import api from '../services/api'
+import { userSchema } from '../services/yupSchemas'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
 
 const Register: React.FC = () => {
 
   let history = useHistory()
-
-  const [data, setData] = useState({
-    fullname: "",
-    cpf: "",
-    gender: "",
-    email: "",
-    password: "",
-    cpassword: "",
-    phone: ""
+  const [backgroundImage, setBackgroundImage] = useState(LoadRandomPicture())
+  
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(userSchema),
   })
-
-  async function submit(e:any){
-    e.preventDefault()
+  
+  const submit = async (data:any) => {
     await api.post("/register", {
       fullname: data.fullname,
       cpf: data.cpf,
@@ -28,16 +26,11 @@ const Register: React.FC = () => {
       email: data.email,
       password: data.password,
       cpassword: data.cpassword,
-      phone: data.phone
+      phone: data.phone,
     }).then(res=>{
+      alert("Cadastro realizado com sucesso!")
       history.push("/login")
     })
-  }
-
-  function handle(e:any){
-    const newData:any = {...data}
-    newData[e.target.id] = e.target.value
-    setData(newData)
   }
 
   return (
@@ -45,45 +38,52 @@ const Register: React.FC = () => {
       <GlobalStyle />
       <Container>
         <RegisterStyle>
-          <Body backgroundImage={ LoadRandomPicture() }>
+          <Body backgroundImage={backgroundImage}>
             <TransparentLayer />
             <a href="/" className="logoBackground"><img src="/images/startup (1).png" alt="logo" className="logoImg" /></a>
             <div className="card-body">
-              <form onSubmit={submit} method="POST" className="box">
+              <form onSubmit={handleSubmit(submit)} method="POST" className="box">
                 <h1 className="">Usuário!</h1>
                 <div className="form-align">
                   <div className="line line-align">
                     <label className="label-align" htmlFor="fullname">Nome: </label>
-                    <input onChange={handle} value={data.fullname} className="input-align" type="text" placeholder="Nome Completo" id="fullname" name="fullname" />
+                    <input className="input-align" type="text" placeholder="Nome Completo" id="fullname" {...register('fullname')}/>
                   </div>
+                  <RequiredField>{errors.fullname?.message}</RequiredField>
                   <div className="line line-align">
                     <label className="label-align" htmlFor="cpf">CPF: </label>
-                    <input onChange={handle} value={data.cpf} type="text" className="input-align" placeholder="000.000.000-00" name="cpf" id="cpf" />
+                    <input type="text" className="input-align" placeholder="000.000.000-00" id="cpf" {...register('cpf')}/>
                   </div>
+                  <RequiredField>{errors.cpf?.message}</RequiredField>
                   <div className="line line-align">
-                    <select className="select-align" name="gender" id="gender" onChange={handle} defaultValue="none">
+                    <select className="select-align" id="gender" defaultValue="none" {...register('gender')}>
                       <option value="none" disabled hidden>Gênero</option>
                       <option value="E">Não informar</option>
                       <option value="M">Masculino</option>
                       <option value="F">Feminino</option>
                     </select>
                   </div>
+                  <RequiredField>{errors.gender?.message}</RequiredField>
                   <div className="line line-align">
                     <label className="label-align" htmlFor="email">Email: </label>
-                    <input onChange={handle} value={data.email} className="input-align" type="email" id="email" placeholder="Email" name="email" />
+                    <input className="input-align" type="email" id="email" placeholder="Email" {...register('email')}/>
                   </div>
+                  <RequiredField>{errors.email?.message}</RequiredField>
                   <div className="line line-align">
                     <label className="label-align" htmlFor="password">Senha: </label>
-                    <input onChange={handle} value={data.password} className="input-align" type="password" id="password" placeholder="Senha" name="password" />
+                    <input className="input-align" type="password" id="password" placeholder="Senha" {...register('password')}/>
                   </div>
+                  <RequiredField>{errors.password?.message}</RequiredField>
                   <div className="line line-align">
                     <label className="label-align" htmlFor="cpassword">Confirmar: </label>
-                    <input onChange={handle} value={data.cpassword} className="input-align" id="cpassword" type="password" placeholder="Confirmar Senha" name="cpassword" />
+                    <input className="input-align" id="cpassword" type="password" placeholder="Confirmar Senha" {...register('cpassword')}/>
                   </div>
+                  <RequiredField>{errors.cpassword && "Senhas devem ser idênticas."}</RequiredField>
                   <div className="line line-align">
                     <label className="label-align" htmlFor="phone">Número: </label>
-                    <input onChange={handle} value={data.phone} className="input-align" id="phone" type="text" placeholder="(**)9****-****" name="phone" />
+                    <input className="input-align" id="phone" type="text" placeholder="(**)9****-****" {...register('phone')}/>
                   </div>
+                  <RequiredField>{errors.phone?.message}</RequiredField>
                 </div>
                 <div className="line">
                   <a href="/registercomp" className="forgot">Cadastro de empresa</a>
