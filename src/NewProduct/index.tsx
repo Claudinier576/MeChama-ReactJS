d5a6cdca6c7b27a9941c05e2c7fbf0967100770a
Claudinier <devneto576@gmail.com>
2021-06-16 14:59:14 -0300

    finalização emp parte 01 cadastro de produtos

--- src/NewProduct/index.tsx
+++ src/NewProduct/index.tsx
@@ -18,24 +18,40 @@ const NewProduct: React.FC = () => {
 
       
   const [data, setData] = useState({
-    ProductName: "",
-    ProductValue: "",
-    descriptionProduct: "",
-    additionalProduct: "",
+    name: "",
+    value: "",
+    description: "",
+    add: "",
+    img:"",
   })
 
     async function SaveNewProduct(){
 
-        axios.post('https://tn-15mechama-com.umbler.net/company/',{},{headers:{
-            
-        }})
+        const DataLocalStorage = localStorage.getItem('tokenUserJWT');
+
+      
+ 
+      
+            await axios.post('https://tn-15mechama-com.umbler.net/company',{
+                ...data
+            }, {
+              headers: {
+                tokenUserJWT: DataLocalStorage
+              }
+            }).then((response) => {if(!response.data.mansageError){
+                window.location.reload();
+            }});
+      
 
         console.log(data);
+
+
         
     }
 
     const [imagePrev, setimagePrev] = useState('https://www.oximig.com.br/arquivos/fotos/interna_1499455670.png');
     function readURL(input: any) {
+
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
@@ -54,26 +70,26 @@ const NewProduct: React.FC = () => {
         <Container>
             <div>
                 <label htmlFor="">Nome do Produto </label>
-                <input type="text" onChange={handle} id="ProductName" placeholder="nome do produto" />
+                <input type="text" onChange={handle} id="name" placeholder="nome do produto" />
             </div>
             <div>
                 <label htmlFor="">Valor do Produto</label>
-                <input type="number" onChange={handle}  id="ProductValue" placeholder="Valor do produto" />
+                <input type="number" onChange={handle}  id="value" placeholder="Valor do produto" />
             </div>
             <div>
                 <label htmlFor="">Descrição do Produto</label>
-                <input type="text" onChange={handle}  id="descriptionProduct" placeholder="Descrição do produto" />
+                <input type="text" onChange={handle}  id="description" placeholder="Descrição do produto" />
             </div>
             <div>
                 <label htmlFor="">Adicionais do Produto</label>
-                <input type="text" onChange={handle} id="additionalProduct" placeholder="Adicionais do produto" />
+                <input type="text" onChange={handle} id="add" placeholder="Adicionais do produto" />
             </div>
             <div className="avatar-upload">
                 <div className="avatar-edit">
-                    <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" onChange={(function () {
-                        readURL(document.getElementById('imageUpload'));
+                    <input type='file' id="img" accept=".png, .jpg, .jpeg" onChange={(function () {
+                        readURL(document.getElementById('img'));
                     })} />
-                    <label htmlFor="imageUpload"></label>
+                    <label htmlFor="img"></label>
                 </div>
                 <div className="avatar-preview">
                     <div id="imagePreview" style={{ backgroundImage: `url(${imagePrev})` }}>
